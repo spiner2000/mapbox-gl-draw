fa4a40af66cf81d4f4a47b38d4d6be9683aeeab2
spiner2000 <1374700+spiner2000@users.noreply.github.com>
2023-03-28 21:03:18 +0200

    Update events.js
    Unable to preventDefault inside passive event listener invocation.
    i3.touchstart @ events.js:83
    fire @ maplibre-gl.js?v=6acfdab6:432
    i2.fire @ setup.js:52
    _firePreventable @ maplibre-gl.js?v=6acfdab6:14848
    touchstart @ maplibre-gl.js?v=6acfdab6:14836
    handleEvent @ maplibre-gl.js?v=6acfdab6:15674
    
    there is an event listener for a touchstart event in a web application, and inside that listener, the code is attempting to call event.preventDefault(). However, the event listener is registered as a "passive" listener, which means that it has explicitly indicated to the browser that it will not call preventDefault(). This is likely done for performance reasons, as passive listeners can improve scrolling performance on touch devices.

--- src/events.js
+++ src/events.js
@@ -80,7 +80,9 @@ export default function(ctx) {
   events.touchstart = function(event) {
     // Prevent emulated mouse events because we will fully handle the touch here.
     // This does not stop the touch events from propogating to mapbox though.
-    event.originalEvent.preventDefault();
+    if (typeof event.originalEvent.cancelable !== "boolean" || event.originalEvent.cancelable) {
+      event.originalEvent.preventDefault();
+    }
     if (!ctx.options.touchEnabled) {
       return;
     }
